ae9777be3d05d064558d76ea7c2efe875765bda4
Matt Warren <Matthew_Warren@homedepot.com>
2017-04-21 11:30:20 -0400

    modified to open email and insert list

--- Project_emails_parser.py
+++ Project_emails_parser.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import webbrowser
 
 project_id='<insert tracker project id here as string>'
 tracker_token='<insert tracker token here as string>'
@@ -10,8 +11,14 @@ response = requests.get(url, headers={'X-TrackerToken': tracker_token})
 
 if(response.ok):
     jData = json.loads(response.content)
+    mail_list = []
     for x in jData:
-        print x['person']['email']
+        # print x['person']['email']
+        mail_list.append(x['person']['email'])
+    for x in mail_list: print x
+    email_to = ";".join(mail_list)
+    # print "mailto:" + str(email_to)
+    webbrowser.open("mailto:" + str(email_to))
 
 else:
     print ("OOPS!, REST response bad :{}")
